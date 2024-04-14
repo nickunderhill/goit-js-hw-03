ed9f8e49cb8e536e8a9f386821983fde095ccc4e
Mykola Podopryhora <m.podopryhora@gmail.com>
2024-04-14 12:37:21 +0300

    hw3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,18 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding) * 2;
-    const borderWidth = parseFloat(border) * 2;
-    return contentWidth + paddingWidth + borderWidth;
+function filterArray(numbers, value) {
+    let filteredNumbers = [];
+
+    for (const number of numbers) {
+        if (number > value) {
+            filteredNumbers.push(number);
+        }
+    }
+    
+    return filteredNumbers;
 }
 
 console.log("Task 3:");
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
